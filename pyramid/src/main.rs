e1976212f6777c5e886b5e0b6d557798cf16e238
Shea Newton <shnewto@gmail.com>
2017-03-31 13:17:15 -0700

    add logic to traverse vector rathe than tree generated from the vector

--- pyramid/src/main.rs
+++ pyramid/src/main.rs
@@ -2,34 +2,31 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 mod node;
 use node::*;
+use std::cmp;
 
 type Pyramid = Vec<Vec<usize>>;
 
 fn main() {
-    // let pyramid = get_pyramid("pyramid.txt");
-    let pyramid = get_pyramid("mini.txt");
-    let mut root = Node { val: pyramid[0][0], l: None, r: None };
+    // let mut pyramid = get_pyramid("pyramid.txt");
+    let mut pyramid = get_pyramid("mini.txt");
+    // let mut root = Node { val: pyramid[0][0], l: None, r: None };
 
-    // populate treeee
-
-
-    populate_binary_tree(&mut root, 1, 0, 1, &pyramid, pyramid.len());
+    // populate_binary_tree(&mut root, 1, 0, 1, &pyramid, pyramid.len());
 
     // print_node(root, 0);
-
-    let max_sum = max_path(root);
+    // let max_sum = max_path_tree(root);
+    let max_sum = max_path_vec(pyramid[0][0], 1, 0, 1, &pyramid, pyramid.len());
 
     println!("max sum: {}", max_sum);
 }
 
-use std::cmp;
 
-fn max_path( root: Node ) -> usize {
+fn max_path_tree( root: Node ) -> usize {
 
     let r_max: usize;
     match root.r {
         Some(node) => {
-            r_max = max_path(*node);
+            r_max = max_path_tree(*node);
         }
         None => { r_max = 0; }
     }
@@ -37,12 +34,24 @@ fn max_path( root: Node ) -> usize {
     let l_max: usize;
     match root.l {
         Some(node) => {
-            l_max = max_path(*node);
+            l_max = max_path_tree(*node);
         }
         None => { l_max = 0; }
     }
 
     root.val + cmp::max(l_max, r_max)
+}
+
+fn max_path_vec(root: usize, depth: usize, lidx: usize, ridx: usize, pyramid: &Pyramid, max_depth: usize) -> usize {
+
+    if depth == max_depth {
+        return root
+    }
+
+    let l_max = max_path_vec(pyramid[depth][lidx], depth+1, lidx, ridx, pyramid, max_depth);
+    let r_max = max_path_vec(pyramid[depth][ridx], depth+1, ridx, ridx + 1, pyramid, max_depth);
+
+    root + cmp::max(l_max, r_max)
 }
 
 fn populate_binary_tree(root: &mut Node, depth: usize, lidx: usize, ridx: usize, pyramid: &Pyramid, max_depth: usize) {
