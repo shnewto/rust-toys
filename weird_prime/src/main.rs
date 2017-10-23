638417c501faea50728a555f6defe583f6337378
Shea Newton <shnewto@gmail.com>
2017-10-22 20:19:11 -0700

    passing example

--- weird_prime/src/main.rs
+++ weird_prime/src/main.rs
@@ -4,33 +4,36 @@
 extern crate test;
 
 fn main() {
-    println!("{:?}", an_seq(10000, vec![]));
-    // println!("{:?}", an_over_average(5));
-    // println!("{:?}", pn(1));
-    // println!("{:?}", max_pn(1));
-//     println!("{:?}", gn(10, vec![]));
-//     println!("{:?}", count_ones(10));
+    println!("an_over_average(9) -- {:?}", an_over_average(9));
+    println!("max_pn {:?}", max_pn(2));
+    println!("count_ones {:?}", count_ones(2));
 }
-
-// println!("max_pn(n) : {}", n);
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use test::Bencher;
 
-    // #[test]
-    // fn returns_expected() {
-    //     assert_eq!(count_ones(1), 1);
-    //     assert_eq!(count_ones(10), 8);
-    //     assert_eq!(count_ones(100), 90);
+    #[test]
+    fn count_ones_returns_expected() {
+        assert_eq!(count_ones(1), 1);
+        assert_eq!(count_ones(10), 8);
+        assert_eq!(count_ones(100), 90);
+    }
 
-    //     assert_eq!(max_pn(1), 5);
-    //     assert_eq!(max_pn(5), 47);
-    //     assert_eq!(max_pn(7), 101);
+    #[test]
+    fn max_pn_returns_expected() {
+        assert_eq!(max_pn(1), 5);
+        assert_eq!(max_pn(5), 47);
+        assert_eq!(max_pn(7), 101);
+    }
 
-    //     assert_eq!(an_over_average(5), 3);
-    // }
+    #[test]
+    fn an_over_average_returns_expected() {
+        assert_eq!(an_over_average(1), 3);
+        assert_eq!(an_over_average(5), 3);
+        assert_eq!(an_over_average(9), 3);
+    }
 
     #[bench]
     fn bench_count_ones(b: &mut Bencher) {
@@ -54,91 +57,94 @@ mod tests {
     }        
 }
 
-
 fn count_ones(n: i64) -> i64 {
     if n == 0 { return 0 }
-    gn(n, vec![]).iter().filter(|&n| *n == 1).count() as i64
+    gn(n).iter().filter(|&n| *n == 1).count() as i64
 }
 
 fn pn(n: i64) -> Vec<i64> {
-
-    let mut count: i64 = n;
-    let mut gn_vec: Vec<i64> = vec![];
+    let mut count: i64 = 2;
+    let mut primes: Vec<i64> = vec![];
+    let mut n_minus_1: i64 = 7;
+    
     loop {
-        gn_vec = gn(count, gn_vec);
-        gn_vec.sort();
-        gn_vec.dedup();        
-        gn_vec.remove(0);        
-        if gn_vec.len() == n as usize { return gn_vec }
-        count += 1;
+        let curr: i64 = n_minus_1 + gcd(count as i64, n_minus_1);
+        let val = (curr - n_minus_1).abs();
+      
+        if (val != 1) && (primes.iter().find(|&&x| x == val) == None) {
+            primes.push(val);   
+        }
+
+        if primes.len() == n as usize {
+            primes.sort();
+            return primes
+        }
+
+        n_minus_1 = curr;      
+        count = count + 1;
     }
 }
 
 fn max_pn(n: i64) -> i64 {
+    if n == 0 { return 0 }
     *pn(n).last().unwrap()
 }
 
 fn an_over_average(n: i64) -> i64 {
+    if n == 0 { return 0 }
     let mut avg: i64 = 0;
-    let ao_s = an_over(n);
-    let denom = ao_s.len() as i64;
-    for elem in ao_s {
+    let an_over_vec = an_over(n);
+    println!("an_over_vec {:?}", an_over_vec);
+    let denom = an_over_vec.len() as i64;
+    for elem in an_over_vec {
         avg += elem;
     }
     
-    avg / (denom + 1)
+    avg / denom 
 }
 
 fn an_over(n: i64) -> Vec<i64> {
-    let pn_vec = pn(n);
+    let mut count: i64 = 2;
+    let mut n_minus_1: i64 = 7;
     let mut ret: Vec<i64> = vec![];
-
-    for (i, elem) in pn_vec.iter().enumerate() {
-        if *elem != 1 {
-            ret.push(elem/(i as i64 + 1));
+    loop {
+        let curr: i64 = n_minus_1 + gcd(count as i64, n_minus_1);
+        let val = (curr - n_minus_1).abs();
+      
+        if val != 1 {
+            ret.push(curr/count);   
         }
-    }
 
-    ret
-}
-
-fn an(n: i64) -> i64 {
-    match n {
-        0 => return 0,
-        1 => return 7,
-        _ => { 
-            let an_val = an(n-1); return an_val + gcd(n, an_val) 
+        if ret.len() == n as usize {
+            return ret
         }
+
+        n_minus_1 = curr;      
+        count = count + 1;
     }
 }
 
-
-fn an_seq(n: i64, mut state: Vec<i64>) -> Vec<i64> {
-    match n {
-        0 => { return state }
-        _ => for i in (state.len() + 1)..(n as usize + 1) { state.push(an(i as i64)); }
+fn an(n: i64) -> Vec<i64> {
+    let mut ret_vec: Vec<i64> = vec![7];
+    let mut n_minus_1: i64 = 7;
+    for i in 2..(n as usize) + 1 {
+        let curr: i64 = n_minus_1 + gcd(i as i64, n_minus_1);
+        ret_vec.push(curr);
+        n_minus_1 = curr;
     }
 
-    state
+    ret_vec
 }
 
-fn gn(n: i64, state: Vec<i64>) -> Vec<i64> {
-    let mut last;
-    let len = state.len();
-
-    if  len > 0 {
-        last = *state.last().unwrap() - 1; // starting from a previous state
-    } else {
-        last = 6; // starting from beginning, 7-6 == 1 at head of list...
-    }
-
-    let ret_vec = an_seq(n, state);
+fn gn(n: i64) -> Vec<i64> {
+    let ret_vec = an(n);
+    let mut last = 6; // hack to get 1. abb(7-6)
     let mut gn_vec: Vec<i64> = vec![];
 
-    for elem in ret_vec[len..(n as usize)].iter() {
+    for elem in ret_vec {
         let val = (elem - last).abs();
         gn_vec.push(val);
-        last = *elem
+        last = elem
     }
 
     gn_vec
